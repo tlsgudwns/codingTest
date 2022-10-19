c1489a1aa9e4c0fdb186121f0f0776e5a0016099
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-10-19 05:10:11 -0700

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,35 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int[] array, int[][] command) {
+   public int[] solution(int[] array, int[][] command) {
         int[] answer = new int[command.length];
-        int[] arrayDup; //= array.clone();
 
         for (int i = 0; i < command.length; i++) {
-            arrayDup = array.clone();
-            int[] arrNew = new int[command[i][1] - command[i][0] + 1];
-            for (int j = command[i][0]; j <= command[i][1]; j++) {
-                arrNew[j - command[i][0]] = array[j - 1];
+            int b = command[i][1];
+            int a = command[i][0];
+            int[] arr = new int[b - a + 1];
+
+            for (int j = a; j <= b; j++) {
+                arr[j - a] = array[j - 1];
             }
-            arrNew = sort(arrNew);
-            answer[i] = arrNew[command[i][2] - 1];
+
+            Arrays.sort(arr);
+            answer[i] = arr[command[i][2] - 1];
+            
         }
+
         return answer;
-    }
-    
-    //sort함수 따로만들기
-    public static int[] sort(int[] arrNew) {
-        int tmp;
-        for (int i = arrNew.length - 2; i >= 0; i--) {
-            for (int j = i ; j < arrNew.length - 1; j++) {
-                if (arrNew[j] > arrNew[j + 1]) {
-                    tmp = arrNew[j];
-                    arrNew[j] = arrNew[j + 1];
-                    arrNew[j + 1] = tmp;
-                } else {
-                    break;
-                }
-            }
-        }
-        return arrNew;
     }
     
 }
