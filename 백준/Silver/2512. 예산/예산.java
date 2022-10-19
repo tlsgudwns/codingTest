8197baaed8aec3d5a484c934dd2f3090cc963535
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-10-19 03:34:46 -0700

    [Silver III] Title: 예산, Time: 176 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,61 +1,48 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
 
-        String s = br.readLine();
-        int n = Integer.parseInt(s);
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] requireList = new int[n];
+        int[] require = new int[n];
+
         for (int i = 0; i < n; i++) {
-            requireList[i] = Integer.parseInt(st.nextToken());
+            require[i] = Integer.parseInt(st.nextToken());
         }
 
-        s = br.readLine();
-        int totalBudget = Integer.parseInt(s);
-        int a = 0;
-        int left = 0;
+        int totalLimit = Integer.parseInt(br.readLine());
+
+        //상한액을 left right mid로 두고 조종해가면서 그합이 totalLimit를 넘지않는 선에서 가장큰값을 구해야한다
+        //upperbound
+
         int max = -1;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(requireList[i], max);
-            a += requireList[i];
+        for (int i = 0; i < require.length; i++) {
+            max = require[i] > max ? require[i] : max;
         }
+        int left = 0;
+        int right = max + 1;
+        int mid = 0;
+        int cnt = 0;
 
-        if (a <= totalBudget) {
-            System.out.println(max);
-            return;
-        }
+        while (left < right) {
+            mid = (left + right) / 2;
+            cnt = 0;
 
-        int right = max;
-        //지방당 최대예산 mid인 경우에 총합이 토탈예산보다 크면 줄이고 작으면 늘리기
-        int answer = 0; //answer는 1곳당 상한액이고 최대예산은 따로 계산해줘야함
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int sum = 0;
-
-            for (int i = 0; i < n; i++) {
-                sum += Math.min(requireList[i], mid);
+            for (int i = 0; i < require.length; i++) {
+                cnt += Math.min(require[i], mid);
             }
 
-            if (sum < totalBudget) {
-                left = mid + 1;
-                answer = Math.max(answer, mid);
-            } else if (sum > totalBudget) {
-                right = mid - 1;
+            if (cnt > totalLimit) { //이 상한액으로 했을때 총예산을 넘어가면 상한액 줄이기
+                right = mid;
             } else {
-                System.out.println(mid);
-                return;
+                left = mid + 1;
             }
-        }//while을 빠져나왓을때 answer에 상한액 저장되어있을듯
-
-
-        System.out.println(answer);
-
+        }
+        System.out.println(left - 1);
     }
 }
-
-
