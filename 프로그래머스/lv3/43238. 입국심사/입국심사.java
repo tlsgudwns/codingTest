7cd7c75aa5464a64cea9bfbfc0a46827a68b1d55
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-10-19 03:09:29 -0700

    [level 3] Title: 입국심사, Time: 0.46 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -1,45 +1,42 @@
+import java.util.Arrays;
+
 class Solution {
-    public static long solution(int n, int[] times) {
-        long answer = 0;
-        int slow = Integer.MAX_VALUE;
-        int fast = Integer.MIN_VALUE;
+   public static long solution(int n, int[] times) {
 
-        for (int i = 0; i < times.length; i++) {
-            slow = Math.max(times[i], slow);
-            fast = Math.min(times[i], fast);
-        }
-        long right = (long)(n / times.length + 1) * slow;
-        long left = (long)(n / times.length) * fast;
+        //가장 느린사람이 기준으로 모든 심사대를 채웠을때 걸리는 시간을 right
 
+        int max = -1;
+        int min = Integer.MAX_VALUE;
+//        for (int item : times) {
+//            max = item > max ? item : max;
+//            min = item < min ? item : min;
+//        }
+//
+//        long right = (n / times.length + 1) * max;
+//        long left = (n / times.length) * min;
+        Arrays.sort(times);
 
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long total = 0;
-
-            for (int i = 0; i < times.length; i++) {
-                total += mid / times[i];
-            }
-
-            if (total < n) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-
-        for (long i = left; i >= right ; i--) {
-            long total = 0;
-            for (int j = 0; j < times.length; j++) {
-                total += i / times[j];
-            }
-            if (total >= n) {
-                return i;
-            }
-        }
-
-
+        long left = times[0];
+        long right = (long)times[times.length - 1] * n;
         
+        long mid = 0;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            long cnt = 0;
+
+            for (int time : times) {
+                cnt += mid / time; //각 심사대별로 mid라는 시간동안 통과시킬수있는 사람의 수를 cnt에 더한다
+            }
+            //최소시간 구하기니까 lower bound 
+            if (cnt >= n) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+
         return left;
     }
 }
