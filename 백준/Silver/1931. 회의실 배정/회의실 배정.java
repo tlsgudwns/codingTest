276565a4a17c50afc1aa438b75fb76d9b6bb9694
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-10-22 16:11:52 +0900

    [Silver I] Title: 회의실 배정, Time: 516 ms, Memory: 43496 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -22,24 +22,36 @@ class Main {
         int cnt = 1;   //times[a][a] 처럼 같은 시간내에 끝나는것들은 제일 마지막에 배정되어야 +1 더받을수 잇을거다
         int i = 0;
         int next = 1;
-        while (i < times.length) {
+//        while (i < times.length) {
+//            next = i + 1;
+//            if (next > times.length - 1) {
+//                System.out.println(cnt);
+//                return;
+//            }
+//            while (times[i][1] > times[next][0]) {
+//                next++;
+//                if (next > times.length - 1) {
+//                    System.out.println(cnt);
+//                    return;
+//                }
+//            }//빠져나오면 next에는 진입가능한 회의의 인덱스가 저장되어있다
+//            cnt++;
+//            i = next;
+//        }
+
+        while (i < times.length - 1) {
             next = i + 1;
-            if (next > times.length - 1) {
-                System.out.println(cnt);
-                return;
-            }
-            while (times[i][1] > times[next][0]) {
+            while (next <= times.length - 1 && times[i][1] > times[next][0]) {
                 next++;
-                if (next > times.length - 1) {
-                    System.out.println(cnt);
-                    return;
-                }
-            }//빠져나오면 next에는 진입가능한 회의의 인덱스가 저장되어있다
+            }
+            if (next == times.length) {//next가 배열의 인덱스를 넘어섬 더 들어갈수있는 회의가 없다
+                break;
+            }
+            //같지않다면 next는 배열의 범위 이내이고 더 회의가 들어갈수있다는뜻이니까 + 1
             cnt++;
             i = next;
         }
 
         System.out.println(cnt);
-        return;
     }
 }
