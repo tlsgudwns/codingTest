9f99eff97c47410dc79eabbb14bb3acb03bf101f
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-10-26 14:21:52 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 25404 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -3,46 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-//        int n = Integer.valueOf(st.nextToken());
-//        int[] arr = new int[n]
-        int[] arr = new int[Integer.valueOf(st.nextToken())];
+
+        int n = Integer.valueOf(st.nextToken());
         int target = Integer.valueOf(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.valueOf(st.nextToken());
         }
 
         int p1 = 0;
         int p2 = 0;
-        int answer = Integer.MAX_VALUE;
         int sum = arr[0];
-
-        while (p2 < arr.length) {
-            if (sum >= target) {
-                answer = p2 - p1 + 1 < answer ? p2 - p1 + 1 : answer;
-                sum = sum - arr[p1++];
-            } else {
-                if (p2 == arr.length - 1) {
-                    break;
-                }
+        int minLen = Integer.MAX_VALUE;
+        while (p2 < arr.length - 1) {
+            if (sum < target) {
                 p2++;
-                sum = sum + arr[p2]; //p2가 가리키고있는 직전까지가 sum의 범위
+                sum += arr[p2];
+            } else if (sum >= target) {
+                minLen = p2 - p1 + 1 < minLen ? p2 - p1 + 1 : minLen;
+                sum -= arr[p1++];
             }
         }
 
-//        while (sum >= target) {
-//            answer = p2 - p1 < answer ? p2 - p1 : answer;
-//            sum = sum - arr[p1++];
-//        }
 
-        if (answer == Integer.MAX_VALUE) {
-            answer = 0;
-        }
-        System.out.println(answer);
+       System.out.println(minLen == Integer.MAX_VALUE ? 0 : minLen);
     }
 }
