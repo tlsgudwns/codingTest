da42798cfb23bd40eb6052b58b48437ec6239d77
tlsgudwns <113477942+tlsgudwns@users.noreply.github.com>
2022-12-07 21:48:22 +0900

    [Gold IV] Title: N-Queen, Time: 4408 ms, Memory: 14716 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,48 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-
-    public static int N;
-    public static int cnt;
-    public static int[] board;
-
+    static int[] map;
+    static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.valueOf(br.readLine());
-        cnt = 0;
-        board = new int[N];
+        int N = Integer.parseInt(br.readLine());
 
-        backTracking(0);
+        map = new int[N];
 
+        backTracking(N, 0);
         System.out.println(cnt);
-
-
     }
 
-    public static void backTracking(int depth) {
+    public static void backTracking(int N, int depth) {
         if (depth == N) {
             cnt++;
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            board[depth] = i;
-            if (isPromising(depth)) {
-                backTracking(depth + 1);
+        boolean can = true;
+
+        for (int i = 0; i < N; i++) { //내가 y축 세로 depth번째 줄에 0~N-1개의 가로좌표들에 새 말을 놓으려할때
+
+            can = true;
+            for (int j = 0; j < depth; j++) { //윗줄에 이미 놓은말들 depth개만큼과 위치검증해야한다
+                if (map[j] == i || Math.abs(map[j] - i) == depth - j) { //대각선에 있거나 같은 x좌표를 가진다면
+                    can = false;                    //이자리에 올수없다
+                    break;
+                }
             }
-        }
-    }
-    public static boolean isPromising(int depth) {
-        for (int i = 0; i < depth; i++) {
-            if (board[depth] == board[i] || depth - i == Math.abs(board[depth] - board[i])) {
-                return false;
+            if (can == true) { //위의 반복문검증을 거쳤는데도 can이 true를 유지했다면 퀸이 들어올수있는 자리니까
+                map[depth] = i;
+                backTracking(N, depth + 1);
             }
+
         }
-        return true;
+
     }
+
 }
